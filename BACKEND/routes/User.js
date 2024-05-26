bc701b13d4f5427e579c8009eeeb9589ff2b8abb
yash52003 <yashchoudhary52003@gmail.com>
2024-05-27 00:59:45 +0530

    Made the protected route for the user , admin and the norman authorisation purpose

--- BACKEND/routes/User.js
+++ BACKEND/routes/User.js
@@ -1,9 +1,32 @@
 const express = require("express");
 const router = express.Router();
 
+
 const {login , signup} = require("../controllers/Auth");
+const {auth , isAdmin , isUser } = require("../middlewares/Auth");
 
 router.post("/login" , login);
 router.post("/signup" , signup);
+
+router.get("/loggedIn" , auth , (req , res) => {
+    res.json({
+        success : true,
+        message : "Welcome you are successfully logged in and can access the courses Now",
+    })
+})
+
+router.get("/user" , auth , isUser , (req , res) => {
+    res.json({
+        success : true,
+        message : "Welcome to the protected route for the User",
+    })
+})
+
+router.get("/admin" , auth , isAdmin , (req , res) => {
+    res.json({
+        success : true,
+        message : "Welcome to the protected route for the admin",
+    })
+})
 
 module.exports = router;
