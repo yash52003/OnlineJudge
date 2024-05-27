3ffbeb5f094754bd73450d2a8130ed5fe4270f82
yash52003 <yashchoudhary52003@gmail.com>
2024-05-27 18:23:50 +0530

    Completed the auth backend part

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,12 +1,14 @@
 //Creating the sever
 const express = require("express");
 const app = express();
+const cors = require('cors');
 
 //Loading the dotenv file and deciding the PORT_NO
 require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 
 //Establishing the middlewares
+app.use(cors());
 app.use(express.json());
 
 //We will also use cookie for authorisation -- Cookie - Parser middleware
